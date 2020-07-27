4aac4977b019fcfd9fae2ea8128fba00616d7c65
zero0dayali <officialali35@gmail.com>
2020-07-27 10:00:58 +0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Ali on 27/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/OneYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneYouApp.swift
+//  Shared
+//
+//  Created by Ali on 27/07/20.
+//
+
+import SwiftUI
+
+@main
+struct OneYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
